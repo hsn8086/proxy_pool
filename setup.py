98b569e850ffd1ae53d57ae814dc11b3cb36eb24
hsn2 <912372447@qq.com>
2022-12-02 12:06:30 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='proxy_pool',
+    version='',
+    packages=['db', 'api', 'test', 'util', 'helper', 'fetcher', 'handler'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
